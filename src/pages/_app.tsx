29320e8d72b5611d58f6b431b6e02b23efe4d1e6
evfjunior <evfjunior@protonmail.me>
2022-10-27 15:02:55 -0300

    fix(tsconfig): fix path aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
-import '@styles/globals.css'
+import 'styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
