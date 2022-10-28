53b7ad9093053b9eddda84931e8cab4360670510
evfjunior <evfjunior@protonmail.me>
2022-10-28 09:55:23 -0300

    feat(theme): create default theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,69 @@
-export const theme = {}
+export const theme = {
+  border: {
+    radius: '0.4rem',
+    rounded: '5rem'
+  },
+  colors: {
+    primary: 'hsl(257, 74%, 57%)',
+    primaryHover: 'hsl(257, 54%, 45%)',
+    primaryActive: 'hsl(257, 52%, 36%)',
+    primaryDisabled: 'hsl(257, 72%, 92%)',
+    primaryTransparent: 'hsla(257, 74%, 57%, 0.04)',
+    primarySemiTransparent: 'hsla(257, 74%, 57%, 0.16)',
+    black: 'hsl(0, 0%, 7%)',
+    blackT: 'hsla(0, 0%, 7%, 0.48)',
+    white: 'hsl(0, 0%, 100%)',
+    whiteS: 'hsla(0, 0%, 100%, 0.88)',
+    whiteTransparent: 'hsla(0, 0%, 100%, 0.12)',
+    whiteSemiTransparent: 'hsla(0, 0%, 100%, 0.16)',
+    grayM: 'hsl(0, 0%, 98%)',
+    grayL: 'hsl(0, 0%, 95%)',
+    grayS: 'hsl(0, 0%, 83%)',
+    gray: 'hsla(0, 0%, 83%, 0.48)',
+    grayK: 'hsl(0, 0%, 42%)',
+    blue: 'hsl(216, 94%, 62%)',
+    green: 'hsl(145, 63%, 42%)',
+    red: 'hsl(2, 66%, 58%)',
+    redS: 'hsl(2, 64%, 47%)',
+    redL: 'hsl(0, 61%, 87%)'
+  },
+  font: {
+    family: {
+      main: "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif",
+      heading:
+        "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif"
+    },
+    size: {
+      medium: '1.6rem'
+    },
+    weight: {
+      normal: 400,
+      bold: 700,
+      black: 900
+    }
+  },
+  grid: {
+    container: '120rem',
+    gutter: '3.2rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  spacings: {
+    tiny: '0.2rem',
+    xxsmall: '0.4rem',
+    xsmall: '0.8rem',
+    small: '1rem',
+    medium: '1.6rem',
+    large: '2rem'
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+    slow: '0.5s ease-in-out'
+  }
+} as const
